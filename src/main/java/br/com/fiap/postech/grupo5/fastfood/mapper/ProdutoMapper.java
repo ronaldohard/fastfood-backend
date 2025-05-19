d28d453a5ffb9e5a276430f048aea0fefefbeb64
Raquel <raquelteixeira502@gmail.com>
2025-05-19 00:30:57 -0300

    fix product

--- src/main/java/br/com/fiap/postech/grupo5/fastfood/mapper/ProdutoMapper.java
+++ src/main/java/br/com/fiap/postech/grupo5/fastfood/mapper/ProdutoMapper.java
@@ -3,6 +3,7 @@ package br.com.fiap.postech.grupo5.fastfood.mapper;
 import br.com.fiap.postech.grupo5.fastfood.dto.ProdutoDTO;
 import br.com.fiap.postech.grupo5.fastfood.model.Produto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -12,8 +13,14 @@ public interface ProdutoMapper {
 
     ProdutoMapper INSTANCE = Mappers.getMapper(ProdutoMapper.class);
 
+    @Mapping(source = "preco",         target = "preco")
+    @Mapping(source = "nome",         target = "nome")
+    @Mapping(source = "tipoProdutoId",         target = "tipoProdutoId")
     Produto toEntity(ProdutoDTO dto);
-
+    
+    @Mapping(source = "preco",         target = "preco")
+    @Mapping(source = "nome",         target = "nome")
+    @Mapping(source = "tipoProdutoId",         target = "tipoProdutoId")
     ProdutoDTO toDto(Produto produto);
 
     List<ProdutoDTO> toDtoList(List<Produto> produtos);
