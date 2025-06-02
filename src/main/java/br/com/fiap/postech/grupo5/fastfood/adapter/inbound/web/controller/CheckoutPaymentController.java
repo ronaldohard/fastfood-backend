aa990daa7dfc816dc82859b661bb8c4b20c7cddd
Raquel <raquelteixeira502@gmail.com>
2025-06-01 22:14:40 -0300

    remove endpoint swagger

--- src/main/java/br/com/fiap/postech/grupo5/fastfood/adapter/inbound/web/controller/CheckoutPaymentController.java
+++ src/main/java/br/com/fiap/postech/grupo5/fastfood/adapter/inbound/web/controller/CheckoutPaymentController.java
@@ -23,19 +23,19 @@ public class CheckoutPaymentController {
     private final PagamentoService service;
     private final MercadoPagoClientAdapter mercadoPagoClientAdapter;
 
-    @Operation(summary = "Resposta da confirmação de pagamento")
-    @PostMapping("/{id}/confirmar")
-    public ResponseEntity<Void> confirmarPagamento(@PathVariable String id) {
-        // Simula chamada para sua API interna
-        Map<String, Object> notificacao = new HashMap<>();
-        notificacao.put("id", id);
-        notificacao.put("status", "approved");
+    // @Operation(summary = "Resposta da confirmação de pagamento")
+    // @PostMapping("/{id}/confirmar")
+    // public ResponseEntity<Void> confirmarPagamento(@PathVariable String id) {
+    //     // Simula chamada para sua API interna
+    //     Map<String, Object> notificacao = new HashMap<>();
+    //     notificacao.put("id", id);
+    //     notificacao.put("status", "approved");
 
-        //todo - servico mock
-        mercadoPagoClientAdapter.confirmar("http://localhost:8080/api/pagamentos/notificacao", notificacao, Void.class);
+    //     //todo - servico mock
+    //     mercadoPagoClientAdapter.confirmar("http://localhost:8080/api/pagamentos/notificacao", notificacao, Void.class);
 
-        return ResponseEntity.ok().build();
-    }
+    //     return ResponseEntity.ok().build();
+    // }
 
 
 
