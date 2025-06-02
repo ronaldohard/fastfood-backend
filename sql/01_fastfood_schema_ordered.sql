97815725a6e6e916612eacad214d9bc06379146e
Raquel <raquelteixeira502@gmail.com>
2025-06-02 14:07:05 -0300

    sequence

--- sql/01_fastfood_schema_ordered.sql
+++ sql/01_fastfood_schema_ordered.sql
@@ -1,426 +1,226 @@
--- DROP SCHEMA public;
-
-COMMENT
-ON SCHEMA public IS 'standard public schema';
-
--- DROP SEQUENCE public.cliente_id_seq;
-
 CREATE SEQUENCE public.cliente_id_seq
     INCREMENT BY 1
     MINVALUE 1
     MAXVALUE 2147483647
     START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
+    CACHE 1
+    NO CYCLE;
 ALTER SEQUENCE public.cliente_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.cliente_id_seq TO postgres;
-
--- DROP SEQUENCE public.ingrediente_id_seq;
+GRANT ALL ON SEQUENCE public.cliente_id_seq TO postgres;
 
 CREATE SEQUENCE public.ingrediente_id_seq
     INCREMENT BY 1
     MINVALUE 1
     MAXVALUE 2147483647
     START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
+    CACHE 1
+    NO CYCLE;
 ALTER SEQUENCE public.ingrediente_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.ingrediente_id_seq TO postgres;
-
--- DROP SEQUENCE public.pagamento_id_seq;
-
-CREATE SEQUENCE public.pagamento_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.pagamento_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.pagamento_id_seq TO postgres;
-
--- DROP SEQUENCE public.pedido_id_seq;
-
-CREATE SEQUENCE public.pedido_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.pedido_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.pedido_id_seq TO postgres;
-
--- DROP SEQUENCE public.pedido_item_customizacao_id_seq;
-
-CREATE SEQUENCE public.pedido_item_customizacao_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.pedido_item_customizacao_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.pedido_item_customizacao_id_seq TO postgres;
-
--- DROP SEQUENCE public.pedido_item_id_seq;
-
-CREATE SEQUENCE public.pedido_item_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.pedido_item_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.pedido_item_id_seq TO postgres;
-
--- DROP SEQUENCE public.produto_id_seq;
-
-CREATE SEQUENCE public.produto_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.produto_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.produto_id_seq TO postgres;
--- DROP SEQUENCE public.produto_ingrediente_id_seq;
-
-CREATE SEQUENCE public.produto_ingrediente_id_seq
-    INCREMENT BY 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.produto_ingrediente_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.produto_ingrediente_id_seq TO postgres;
-
--- DROP SEQUENCE public.tipo_pagamento_id_seq;
+GRANT ALL ON SEQUENCE public.ingrediente_id_seq TO postgres;
 
 CREATE SEQUENCE public.tipo_pagamento_id_seq
     INCREMENT BY 1
     MINVALUE 1
     MAXVALUE 2147483647
     START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
+    CACHE 1
+    NO CYCLE;
 ALTER SEQUENCE public.tipo_pagamento_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.tipo_pagamento_id_seq TO postgres;
-
--- DROP SEQUENCE public.tipo_produto_id_seq;
+GRANT ALL ON SEQUENCE public.tipo_pagamento_id_seq TO postgres;
 
 CREATE SEQUENCE public.tipo_produto_id_seq
     INCREMENT BY 1
     MINVALUE 1
     MAXVALUE 2147483647
     START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
+    CACHE 1
+    NO CYCLE;
 ALTER SEQUENCE public.tipo_produto_id_seq OWNER TO postgres;
-GRANT
-ALL
-ON SEQUENCE public.tipo_produto_id_seq TO postgres;
--- public.cliente definição
+GRANT ALL ON SEQUENCE public.tipo_produto_id_seq TO postgres;
 
--- Drop table
+CREATE SEQUENCE public.pagamento_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    START 1
+    CACHE 1
+    NO CYCLE;
+ALTER SEQUENCE public.pagamento_id_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.pagamento_id_seq TO postgres;
 
--- DROP TABLE public.cliente;
+CREATE SEQUENCE public.pedido_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    START 1
+    CACHE 1
+    NO CYCLE;
+ALTER SEQUENCE public.pedido_id_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.pedido_id_seq TO postgres;
+
+CREATE SEQUENCE public.pedido_item_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    START 1
+    CACHE 1
+    NO CYCLE;
+ALTER SEQUENCE public.pedido_item_id_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.pedido_item_id_seq TO postgres;
+
+CREATE SEQUENCE public.pedido_item_customizacao_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    START 1
+    CACHE 1
+    NO CYCLE;
+ALTER SEQUENCE public.pedido_item_customizacao_id_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.pedido_item_customizacao_id_seq TO postgres;
+
+CREATE SEQUENCE public.produto_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    START 1
+    CACHE 1
+    NO CYCLE;
+ALTER SEQUENCE public.produto_id_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.produto_id_seq TO postgres;
+
+CREATE SEQUENCE public.produto_ingrediente_id_seq
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    START 1
+    CACHE 1
+    NO CYCLE;
+ALTER SEQUENCE public.produto_ingrediente_id_seq OWNER TO postgres;
+GRANT ALL ON SEQUENCE public.produto_ingrediente_id_seq TO postgres;
 
 CREATE TABLE public.cliente
 (
-    id   serial4 NOT NULL,
+    id   integer NOT NULL DEFAULT nextval('public.cliente_id_seq'),
     nome varchar(100) NULL,
-    cel  varchar(20) NULL,
-    cpf  varchar(14) NULL,
+    cel  varchar(20)  NULL,
+    cpf  varchar(14)  NULL,
     CONSTRAINT cliente_pkey PRIMARY KEY (id)
 );
-
--- Permissions
-
 ALTER TABLE public.cliente OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.cliente TO postgres;
-
-
--- public.ingrediente definição
-
--- Drop table
-
--- DROP TABLE public.ingrediente;
+GRANT ALL ON TABLE public.cliente TO postgres;
 
 CREATE TABLE public.ingrediente
 (
-    id    serial4        NOT NULL,
+    id    integer NOT NULL DEFAULT nextval('public.ingrediente_id_seq'),
     nome  varchar(100)   NOT NULL,
-    preco numeric(10, 2) NOT NULL,
+    preco numeric(10,2)  NOT NULL,
     CONSTRAINT ingrediente_pkey PRIMARY KEY (id)
 );
-
--- Permissions
-
 ALTER TABLE public.ingrediente OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.ingrediente TO postgres;
-
-
--- public.tipo_pagamento definição
-
--- Drop table
-
--- DROP TABLE public.tipo_pagamento;
+GRANT ALL ON TABLE public.ingrediente TO postgres;
 
 CREATE TABLE public.tipo_pagamento
 (
-    id   serial4     NOT NULL,
-    tipo varchar(50) NOT NULL,
+    id   integer NOT NULL DEFAULT nextval('public.tipo_pagamento_id_seq'),
+    tipo varchar(50)  NOT NULL,
     CONSTRAINT tipo_pagamento_pkey PRIMARY KEY (id)
 );
-
--- Permissions
-
 ALTER TABLE public.tipo_pagamento OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.tipo_pagamento TO postgres;
-
-
--- public.tipo_produto definição
-
--- Drop table
-
--- DROP TABLE public.tipo_produto;
+GRANT ALL ON TABLE public.tipo_pagamento TO postgres;
 
 CREATE TABLE public.tipo_produto
 (
-    id   serial4     NOT NULL,
-    nome varchar(50) NOT NULL,
+    id   integer NOT NULL DEFAULT nextval('public.tipo_produto_id_seq'),
+    nome varchar(50)  NOT NULL,
     CONSTRAINT tipo_produto_pkey PRIMARY KEY (id)
 );
-
--- Permissions
-
 ALTER TABLE public.tipo_produto OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.tipo_produto TO postgres;
-
-
--- public.pagamento definição
-
--- Drop table
-
--- DROP TABLE public.pagamento;
+GRANT ALL ON TABLE public.tipo_produto TO postgres;
 
 CREATE TABLE public.pagamento
 (
-    id                serial4        NOT NULL,
-    pedido_id int8 NOT NULL,
-    tipo_pagamento_id int4           NOT NULL,
-    status            varchar(20)    NOT NULL,
-    valor_total       numeric(10, 2) NOT NULL,
-    qr_code_url       text NULL,
+    id                integer   NOT NULL DEFAULT nextval('public.pagamento_id_seq'),
+    pedido_id         int8      NOT NULL,
+    tipo_pagamento_id int4      NOT NULL,
+    status            varchar(20) NOT NULL,
+    valor_total       numeric(10,2) NOT NULL,
+    qr_code_url       text      NULL,
     criado_em         timestamp DEFAULT now() NULL,
     CONSTRAINT pagamento_pkey PRIMARY KEY (id),
     CONSTRAINT fk_pagamento_tipo FOREIGN KEY (tipo_pagamento_id) REFERENCES public.tipo_pagamento (id),
     CONSTRAINT pagamento_unique UNIQUE (pedido_id)
-
 );
-
--- Permissions
-
 ALTER TABLE public.pagamento OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.pagamento TO postgres;
-
-
--- public.pedido definição
-
--- Drop table
-
--- DROP TABLE public.pedido;
+GRANT ALL ON TABLE public.pagamento TO postgres;
 
 CREATE TABLE public.pedido
 (
-    id           serial4        NOT NULL,
-    cliente_id   int4 NULL,
-    pagamento_id int4 NULL,
+    id           integer   NOT NULL DEFAULT nextval('public.pedido_id_seq'),
+    cliente_id   int4      NULL,
+    pagamento_id int4      NULL,
     "data"       timestamp DEFAULT now() NULL,
-    valor_total  numeric(10, 2) NOT NULL,
-    status       varchar NULL,
+    valor_total  numeric(10,2)  NOT NULL,
+    status       varchar   NULL,
     CONSTRAINT pedido_pkey PRIMARY KEY (id),
     CONSTRAINT fk_pedido_cliente FOREIGN KEY (cliente_id) REFERENCES public.cliente (id),
     CONSTRAINT fk_pedido_pagamento FOREIGN KEY (pagamento_id) REFERENCES public.pagamento (id)
 );
-
--- Permissions
-
 ALTER TABLE public.pedido OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.pedido TO postgres;
-
-
--- public.produto definição
-
--- Drop table
-
--- DROP TABLE public.produto;
+GRANT ALL ON TABLE public.pedido TO postgres;
 
 CREATE TABLE public.produto
 (
-    id              serial4        NOT NULL,
-    nome            varchar(100)   NOT NULL,
-    tipo_produto_id int4           NOT NULL,
-    preco           numeric(10, 2) NOT NULL,
+    id              integer NOT NULL DEFAULT nextval('public.produto_id_seq'),
+    nome            varchar(100) NOT NULL,
+    tipo_produto_id int4   NOT NULL,
+    preco           numeric(10,2) NOT NULL,
     CONSTRAINT produto_pkey PRIMARY KEY (id),
     CONSTRAINT fk_produto_tipo FOREIGN KEY (tipo_produto_id) REFERENCES public.tipo_produto (id)
 );
-
--- Permissions
-
 ALTER TABLE public.produto OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.produto TO postgres;
-
-
--- public.produto_ingrediente definição
-
--- Drop table
-
--- DROP TABLE public.produto_ingrediente;
+GRANT ALL ON TABLE public.produto TO postgres;
 
 CREATE TABLE public.produto_ingrediente
 (
-    id             serial4 NOT NULL,
+    id             integer NOT NULL DEFAULT nextval('public.produto_ingrediente_id_seq'),
     produto_id     int4    NOT NULL,
     ingrediente_id int4    NOT NULL,
     CONSTRAINT produto_ingrediente_pkey PRIMARY KEY (id),
     CONSTRAINT fk_produtoingrediente_ingrediente FOREIGN KEY (ingrediente_id) REFERENCES public.ingrediente (id),
-    CONSTRAINT fk_produtoingrediente_produto FOREIGN KEY (produto_id) REFERENCES public.produto (id)
+    CONSTRAINT fk_produtoingrediente_produto    FOREIGN KEY (produto_id)     REFERENCES public.produto (id)
 );
-
--- Permissions
-
 ALTER TABLE public.produto_ingrediente OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.produto_ingrediente TO postgres;
-
-
--- public.pedido_item definição
-
--- Drop table
-
--- DROP TABLE public.pedido_item;
+GRANT ALL ON TABLE public.produto_ingrediente TO postgres;
 
 CREATE TABLE public.pedido_item
 (
-    id         serial4        NOT NULL,
-    pedido_id  int4           NOT NULL,
-    produto_id int4           NOT NULL,
-    quantidade int4           NOT NULL,
-    preco      numeric(10, 2) NOT NULL,
+    id         integer   NOT NULL DEFAULT nextval('public.pedido_item_id_seq'),
+    pedido_id  int4      NOT NULL,
+    produto_id int4      NOT NULL,
+    quantidade int4      NOT NULL,
+    preco      numeric(10,2) NOT NULL,
     CONSTRAINT pedido_item_pkey PRIMARY KEY (id),
-    CONSTRAINT fk_pedidoitem_pedido FOREIGN KEY (pedido_id) REFERENCES public.pedido (id),
+    CONSTRAINT fk_pedidoitem_pedido  FOREIGN KEY (pedido_id)  REFERENCES public.pedido (id),
     CONSTRAINT fk_pedidoitem_produto FOREIGN KEY (produto_id) REFERENCES public.produto (id)
 );
-
--- Permissions
-
 ALTER TABLE public.pedido_item OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.pedido_item TO postgres;
-
-
--- public.pedido_item_customizacao definição
-
--- Drop table
-
--- DROP TABLE public.pedido_item_customizacao;
+GRANT ALL ON TABLE public.pedido_item TO postgres;
 
 CREATE TABLE public.pedido_item_customizacao
 (
-    id             serial4        NOT NULL,
-    pedido_item_id int4           NOT NULL,
-    ingrediente_id int4           NOT NULL,
-    tipo           varchar(20)    NOT NULL,
-    preco          numeric(10, 2) NOT NULL,
+    id             integer NOT NULL DEFAULT nextval('public.pedido_item_customizacao_id_seq'),
+    pedido_item_id int4    NOT NULL,
+    ingrediente_id int4    NOT NULL,
+    tipo           varchar(20)      NOT NULL,
+    preco          numeric(10,2)    NOT NULL,
     CONSTRAINT pedido_item_customizacao_pkey PRIMARY KEY (id),
-    CONSTRAINT pedido_item_customizacao_tipo_check CHECK (((tipo)::text = ANY ((ARRAY['addition':: character varying, 'removal':: character varying])::text[])
-) ),
-	CONSTRAINT fk_customizacao_ingrediente FOREIGN KEY (ingrediente_id) REFERENCES public.ingrediente(id),
-	CONSTRAINT fk_customizacao_item FOREIGN KEY (pedido_item_id) REFERENCES public.pedido_item(id)
+    CONSTRAINT pedido_item_customizacao_tipo_check CHECK ( (tipo)::text = ANY (ARRAY['addition'::character varying, 'removal'::character varying]) ),
+    CONSTRAINT fk_customizacao_ingrediente FOREIGN KEY (ingrediente_id)   REFERENCES public.ingrediente (id),
+    CONSTRAINT fk_customizacao_item       FOREIGN KEY (pedido_item_id)    REFERENCES public.pedido_item (id)
 );
-
--- Permissions
-
 ALTER TABLE public.pedido_item_customizacao OWNER TO postgres;
-GRANT
-ALL
-ON TABLE public.pedido_item_customizacao TO postgres;
+GRANT ALL ON TABLE public.pedido_item_customizacao TO postgres;
 
 
-
-
--- Permissions
-
-GRANT ALL
-ON SCHEMA public TO pg_database_owner;
-GRANT USAGE ON SCHEMA
-public TO public;
+GRANT ALL ON SCHEMA public TO pg_database_owner;
+GRANT USAGE ON SCHEMA public TO public;
--- sql/02_fastfood_insert.sql
+++ sql/02_fastfood_insert.sql
@@ -1,48 +1,127 @@
 INSERT INTO tipo_produto (id, nome)
-VALUES (1, 'Hamburguer'),
-       (2, 'Acompanhamento'),
-       (3, 'Sobremesa'),
-       (4, 'Bebida');
+VALUES
+    (nextval('public.tipo_produto_id_seq'), 'Hamburguer'),
+    (nextval('public.tipo_produto_id_seq'), 'Acompanhamento'),
+    (nextval('public.tipo_produto_id_seq'), 'Sobremesa'),
+    (nextval('public.tipo_produto_id_seq'), 'Bebida');
+
 INSERT INTO produto (id, nome, tipo_produto_id, preco)
-VALUES (1, 'X-Hamburguer', 1, 10.00),
-       (2, 'X-Bacon', 1, 12.00),
-       (3, 'Fritas', 2, 3.00),
-       (4, 'Batata Rústica', 2, 4.50),
-       (5, 'Pudim', 3, 5.00),
-       (6, 'Sorvete', 3, 6.00),
-       (7, 'Coca', 4, 5.00),
-       (8, 'Pepsi', 4, 4.50);
+VALUES
+    (nextval('public.produto_id_seq'), 'X-Hamburguer',   1, 10.00),
+    (nextval('public.produto_id_seq'), 'X-Bacon',        1, 12.00),
+    (nextval('public.produto_id_seq'), 'Fritas',         2,  3.00),
+    (nextval('public.produto_id_seq'), 'Batata Rústica', 2,  4.50),
+    (nextval('public.produto_id_seq'), 'Pudim',          3,  5.00),
+    (nextval('public.produto_id_seq'), 'Sorvete',        3,  6.00),
+    (nextval('public.produto_id_seq'), 'Coca',           4,  5.00),
+    (nextval('public.produto_id_seq'), 'Pepsi',          4,  4.50);
+
 INSERT INTO ingrediente (id, nome, preco)
-VALUES (1, 'Salada', 1.00),
-       (2, 'Queijo', 1.00),
-       (3, 'Tomate', 1.00),
-       (4, 'Bacon', 2.00),
-       (5, 'Ovo', 1.50);
+VALUES
+    (nextval('public.ingrediente_id_seq'), 'Salada', 1.00),
+    (nextval('public.ingrediente_id_seq'), 'Queijo', 1.00),
+    (nextval('public.ingrediente_id_seq'), 'Tomate', 1.00),
+    (nextval('public.ingrediente_id_seq'), 'Bacon',  2.00),
+    (nextval('public.ingrediente_id_seq'), 'Ovo',    1.50);
+
 INSERT INTO produto_ingrediente (id, produto_id, ingrediente_id)
-VALUES (1, 1, 1),
-       (2, 1, 2),
-       (3, 1, 3),
-       (4, 2, 2),
-       (5, 2, 4);
+VALUES
+    (nextval('public.produto_ingrediente_id_seq'), 1, 1),
+    (nextval('public.produto_ingrediente_id_seq'), 1, 2),
+    (nextval('public.produto_ingrediente_id_seq'), 1, 3),
+    (nextval('public.produto_ingrediente_id_seq'), 2, 2),
+    (nextval('public.produto_ingrediente_id_seq'), 2, 4);
+
 INSERT INTO cliente (id, nome, cel, cpf)
-VALUES (1, 'Ronaldo', '11912345678', '11122233344');
+VALUES
+    (nextval('public.cliente_id_seq'), 'Ronaldo', '11912345678', '11122233344');
+
 INSERT INTO tipo_pagamento (id, tipo)
-VALUES (1, 'QR CODE'),
-       (2, 'Cartão');
+VALUES
+    (nextval('public.tipo_pagamento_id_seq'), 'QR CODE'),
+    (nextval('public.tipo_pagamento_id_seq'), 'Cartão');
+
 INSERT INTO pagamento (id, tipo_pagamento_id, status, valor_total, qr_code_url, criado_em)
-VALUES (1, 1, 'pending', 58.00, 'https://pagamento.exemplo.com/qrcode/123456', '2025-05-15T21:30:00');
+VALUES
+    (nextval('public.pagamento_id_seq'), 1, 'pending', 58.00, 'https://pagamento.exemplo.com/qrcode/123456', '2025-05-15T21:30:00');
+
 INSERT INTO pedido (id, cliente_id, pagamento_id, data, valor_total)
-VALUES (1, 1, 1, '2025-05-15T21:30:00', 58.00);
+VALUES
+    (nextval('public.pedido_id_seq'), 1, 1, '2025-05-15T21:30:00', 58.00);
+
 INSERT INTO pedido_item (id, pedido_id, produto_id, quantidade, preco)
-VALUES (1, 1, 1, 1, 10.00),
-       (2, 1, 2, 1, 12.00),
-       (3, 1, 3, 1, 3.00),
-       (4, 1, 4, 1, 4.50),
-       (5, 1, 5, 1, 5.00),
-       (6, 1, 6, 1, 6.00),
-       (7, 1, 7, 1, 5.00),
-       (8, 1, 8, 1, 4.50);
+VALUES
+    (nextval('public.pedido_item_id_seq'), 1, 1, 1, 10.00),
+    (nextval('public.pedido_item_id_seq'), 1, 2, 1, 12.00),
+    (nextval('public.pedido_item_id_seq'), 1, 3, 1,  3.00),
+    (nextval('public.pedido_item_id_seq'), 1, 4, 1,  4.50),
+    (nextval('public.pedido_item_id_seq'), 1, 5, 1,  5.00),
+    (nextval('public.pedido_item_id_seq'), 1, 6, 1,  6.00),
+    (nextval('public.pedido_item_id_seq'), 1, 7, 1,  5.00),
+    (nextval('public.pedido_item_id_seq'), 1, 8, 1,  4.50);
+
 INSERT INTO pedido_item_customizacao (id, pedido_item_id, ingrediente_id, tipo, preco)
-VALUES (1, 1, 4, 'addition', 2.00),
-       (2, 1, 5, 'addition', 1.50),
-       (3, 1, 1, 'removal', 0.00);
+VALUES
+    (nextval('public.pedido_item_customizacao_id_seq'), 1, 4, 'addition', 2.00),
+    (nextval('public.pedido_item_customizacao_id_seq'), 1, 5, 'addition', 1.50),
+    (nextval('public.pedido_item_customizacao_id_seq'), 1, 1, 'removal',  0.00);
+
+SELECT setval(
+  'public.cliente_id_seq',
+  (SELECT MAX(id) FROM public.cliente),
+  true
+);
+
+SELECT setval(
+  'public.ingrediente_id_seq',
+  (SELECT MAX(id) FROM public.ingrediente),
+  true
+);
+
+SELECT setval(
+  'public.tipo_pagamento_id_seq',
+  (SELECT MAX(id) FROM public.tipo_pagamento),
+  true
+);
+
+SELECT setval(
+  'public.tipo_produto_id_seq',
+  (SELECT MAX(id) FROM public.tipo_produto),
+  true
+);
+
+SELECT setval(
+  'public.pagamento_id_seq',
+  (SELECT MAX(id) FROM public.pagamento),
+  true
+);
+
+SELECT setval(
+  'public.pedido_id_seq',
+  (SELECT MAX(id) FROM public.pedido),
+  true
+);
+
+SELECT setval(
+  'public.pedido_item_id_seq',
+  (SELECT MAX(id) FROM public.pedido_item),
+  true
+);
+
+SELECT setval(
+  'public.pedido_item_customizacao_id_seq',
+  (SELECT MAX(id) FROM public.pedido_item_customizacao),
+  true
+);
+
+SELECT setval(
+  'public.produto_id_seq',
+  (SELECT MAX(id) FROM public.produto),
+  true
+);
+
+SELECT setval(
+  'public.produto_ingrediente_id_seq',
+  (SELECT MAX(id) FROM public.produto_ingrediente),
+  true
+);
